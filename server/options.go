1fa50cb5bda71a665f64b88f7cc6126dd24dddea
mason <chenmusheng@laoyuegou.com>
2019-11-27 12:52:06 +0800

    default

--- server/options.go
+++ server/options.go
@@ -46,7 +46,7 @@ type Option struct {
 }
 
 var defaultOption = &Option{
-	MaxConnIdleTime: 1000,
-	JobChanSize:     10000,
-	WorkerNum:       500,
+	MaxConnIdleTime: 10,
+	JobChanSize:     50000,
+	WorkerNum:       1000,
 }
